88f00b60143505f7d4f628b1c94e99a37b736323
s0ubhik <soubhik1809@gmail.com>
2023-12-16 10:48:36 +0530

    update hook

--- projects/app/src/pages/api/admin/update.ts
+++ projects/app/src/pages/api/admin/update.ts
@@ -4,11 +4,12 @@ const execAsync = promisify(exec);
 
 export default async function handler(req, res) {
   try {
+    // run coammnd
     const command = 'bash $HOME/update > $HOME/update.log & ';
     await execAsync(command);
-    res.status(200).json({ status: true });
+    res.status(200).json({ status: true, message: 'Updating...' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ status: false, error: 'Internal Server Error' });
+    res.status(500).json({ status: false, message: 'Internal Server Error :(' });
   }
 }
