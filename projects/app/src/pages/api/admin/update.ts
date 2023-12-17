f443024d5290eb3d1eaff3ccd7561d928d5935f4
Soubhik Biswas <55374257+s0ubhik@users.noreply.github.com>
2023-12-17 14:14:06 +0530

    Update update.ts

--- projects/app/src/pages/api/admin/update.ts
+++ projects/app/src/pages/api/admin/update.ts
@@ -3,7 +3,7 @@ import { promisify } from 'util';
 
 export default async function handler(req, res) {
   try {
-    const command = 'bash $HOME/update > $HOME/update.log & ';
+    const command = "sudo su soubhik -c 'bash $HOME/update > $HOME/update.log &'";
     const childProcess = spawn(command, {
       shell: true,
       detached: true,
